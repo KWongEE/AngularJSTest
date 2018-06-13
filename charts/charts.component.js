35bdd506002d1e28e526ad0f8998016344394f44
Kevin <kwongca.95@gmail.com>
2018-06-12 22:20:55 -0400

    Able to see data in app.js using rootScope broadcast

--- charts/charts.component.js
+++ charts/charts.component.js
@@ -1,6 +1,8 @@
+// var app = angular.module('app', ["chart.js"]);
 function ChartsController() {
+
 }
-app.component('postsList', {
+app.component('charts', {
   templateUrl: 'charts/charts.html',
   controller: ChartsController,
   controllerAs: 'vm'
@@ -10,7 +12,9 @@ app.directive("importSheetJs", [SheetJSImportDirective]);
 
 function SheetJSImportDirective() {
   return {
-    scope: { opts: '=' },
+    scope: {
+      opts: '='
+    },
     link: function ($scope, $elm) {
       $elm.on('change', function (changeEvent) {
         var reader = new FileReader();
@@ -22,17 +26,54 @@ function SheetJSImportDirective() {
           var sheet_name_list = workbook.SheetNames;
           var airlines = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]])
           var dictionary = {};
+
           airlines.forEach(function(element){
-            if (dictionary[element["Airline Name"]] == null){
+            if (dictionary[element["Airline Name"]] == null) {
               dictionary[element["Airline Name"]] = [element];
             } else {
               dictionary[element["Airline Name"]].push(element);
             }
-            console.log(dictionary)
-          })
+          });
+
+          $scope.$root.$broadcast('data', dictionary);
         };
         reader.readAsBinaryString(changeEvent.target.files[0]);
       });
     }
   };
 }
+
+
+// for (var key in $scope.data) {
+//   if ($scope.data.hasOwnProperty(key)) {
+//     airline_names.push(key);
+//     debugger;
+//     console.log(airline_names);
+//   }
+// }
+
+// function bargraph(){
+//   return {
+//     app.controller('StackedBarCtrl', ['$scope', function ($scope) {
+//       $scope.labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+//       $scope.type = 'StackedBar';
+//       $scope.series = ['2015', '2017'];
+//       $scope.options = {
+//         scales: {
+//           xAxes: [{
+//             stacked: true,
+//           }],
+//           yAxes: [{
+//             stacked: true
+//           }]
+//         }
+//       };
+//
+//       $scope.data = [
+//         [65, 59, 90, 81, 56, 55, 40],
+//         [28, 48, 40, 19, 96, 27, 160]
+//       ];
+//     }]);
+//
+//   }
+// }
