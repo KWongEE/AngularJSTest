9fe6617a72de4a43f2036ce86e19ab9a01ed941d
Kevin <kwongca.95@gmail.com>
2018-06-14 01:55:42 -0400

    Fixed Chart

--- app.js
+++ app.js
@@ -16,68 +16,56 @@ app.config(function (ChartJsProvider) {
 
 app.controller('LineCtrl', function ($scope, $rootScope){
   $rootScope.$on('data', function(event, data) {
+    delete data["undefined"];
+
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     var airline_names = [];
+    var dataset = [];
     for (var key in data) {
+      //key is airline name
       if (key != "undefined") {
-      if (data.hasOwnProperty(key)) {
-        airline_names.push(key);
-      }}
-    }
+        if (data.hasOwnProperty(key)) {
+          airline_names.push(key);
 
-    var incident_month = [];
-    for (var key in data) {
-      if (key != "undefined"){
-        if (data.hasOwnProperty(key)){
           //Each airline will be given an array for $scope.data charts
-          incident_month = Object.values(data);
-          Object.values(data).forEach(function(airline){
-            //Inside the specific airline array
-            debugger;
-            var counter =0;
+          dataset = Object.values(data);
+
+
+          for(var i = 0; i < dataset.length; i++) {
+            var airline = dataset[i];
+
             var monthlyamount = [];
+            var airline_months = [0,0,0,0,0,0,0,0,0,0,0,0];
+            var emptyarray = [...Array(12)].map(e => Array(0));
+            //empty array is to store every close amount value per month in order to average
+
             airline.forEach(function(incident){
-
               var closeamount = incident["Close Amount"]
-              var current_month = incident["Incident Date"].split('/')[0]
-              if (incident["Incident Date"].split('/')[0] == current_month) {
-                counter +=1;
+                var current_month = incident["Incident Date"].split('/')[0]
                 if (closeamount != "-"){
-                 closeamount = Number(closeamount.replace(/[^0-9\.-]+/g,""));
-                 closeamount += closeamount;
-                 monthlyamount.push(closeamount);
+                  closeamount = Number(closeamount.replace(/[^0-9\.-]+/g,""));
+                  month_index = parseInt(current_month) - 1
+                  emptyarray[month_index].push(closeamount);
+                  //Pushes all close amounts into emptyarray per month
                 }
-              }
-              function getSum(total,num){
-                return total+ num;
-              }
-              debugger;
-              monthlyamount.reduce(getSum);
-              closeamount = monthlyamount.reduce(getSum)/monthlyamount.length;
-              incident_month[0] = closeamount;
-              debugger;
-
             })
+            function getSum(total,num){
+              return total+ num;
+            }
+            var averages = emptyarray.map(month => {
+              if (month.length === 0) { return 0; }
+              return month.reduce(getSum)/month.length;
+            });
 
-
-          })
-
-
-          // debugger;
-          // data[key].forEach(function(incident){
-          //   incident_month.push(incident["Incident Date"].split('/')[0])
-          //   ;
-          // })
+            dataset[i] = averages;
+          }
         }
       }
     }
    $scope.labels = months;
    $scope.series = airline_names;
-   debugger;
-   $scope.data = [
-     [65, 59, 80, 81, 56, 55, 40],
-     [28, 48, 40, 19, 86, 27, 90]
-   ];
+   $scope.data = dataset;
+
    $scope.onClick = function (points, evt) {
      console.log(points, evt);
    };
@@ -101,52 +89,3 @@ app.controller('LineCtrl', function ($scope, $rootScope){
    };
  });
  });
-
-
-
-
-
-
-
-
-
-//
-//   $rootScope.$on('data', function(event, data) {
-//     console.log(data);
-      // var airline_names = [];
-      // for (var key in data) {
-      //   if (key != "undefined") {
-      //   if (data.hasOwnProperty(key)) {
-      //     airline_names.push(key);
-      //   }}
-      // }
-//
-//
-//       var incident_month = [];
-//       for (var key in data) {
-//         if (key != "undefined"){
-//           if (data.hasOwnProperty(key)){
-//             data[key].forEach(function(incident){
-//               incident_month.push(incident["Incident Date"].split('/')[0])
-//             })
-//           }
-//         }
-//       }
-//     debugger;
-//
-//     $scope.labels = ["January", "February", "March", "April", "May", "June", "July","August","September","October","November","December"];
-//     $scope.type = 'StackedBar';
-//     $scope.series = airline_names
-//     $scope.options = {
-//       scales: {
-//         xAxes: [{
-//           stacked: true,
-//         }],
-//         yAxes: [{
-//           stacked: true
-//         }]
-//       }
-//     };
-//
-//   });
-// });
